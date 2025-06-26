9ecfbcfe8470356d6351fa5fe356ee9db65a9f41
boricsk <borics.krisztian@gmail.com>
2025-06-26 19:38:29 +0200

    250626

---
+++ C#/codewars_c#/026_Boogle_Word_Checker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _026_Boogle_Word_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
